415f8234f878c29934cffebd7838c473957f0c4c
João Gularte <joaomgularte5555@gmail.com>
2025-10-04 17:17:48 -0300

    header e footer genéricos

--- src/app/layout/footer/footer.ts
+++ src/app/layout/footer/footer.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
 })
 export class Footer {
+  public currentYear: number;
 
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
 }

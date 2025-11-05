84025877057b1dc661683a850f98994662e6e7d5
João Gularte <joaomgularte5555@gmail.com>
2025-11-05 15:16:56 -0300

    primeira versão train-page

--- src/app/pages/train-service/train-service.ts
+++ src/app/pages/train-service/train-service.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSection } from '../../components/hero-section/hero-section';
+import { CtaSection } from '../../components/cta-section/cta-section';
 
 @Component({
   selector: 'app-train-service',
-  imports: [],
+  imports: [HeroSection, CtaSection],
   templateUrl: './train-service.html',
   styleUrl: './train-service.scss'
 })

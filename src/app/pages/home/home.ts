fe5b013ee9c83dffe879839e9d34fe874ff9d6f0
João Gularte <107907952+OndeJoaoGularte@users.noreply.github.com>
2025-11-21 12:49:30 -0300

    primeira versão home-page

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CtaSection } from '../../components/cta-section/cta-section';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CommonModule, RouterLink, CtaSection],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
